28cac882fb8a2be0303b5fd9b832f276cf7e57a4
JJ Ferman <fermanjj@gmail.com>
2019-09-12 11:27:59 -0700

    moving checks in scripts

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-./load_env.sh
+# check if awscli is installed
+aws --version >> /dev/null || ( echo "awscli not installed" && exit 1 )
 
+# check if samcli is installed
+sam --version >> /dev/null || ( echo "samcli not installed" && exit 1 )
+
+# load the env file
+set -a
+. ./local_config.env || ( echo "local_config.env file doesn't exist, exiting" && exit 0 )
+set +a
+
+# run the deploy steps
 sam package --profile "${PROFILE}" --region "${REGION}" --template-file ./template.yaml --s3-bucket "${S3_BUCKET}" --output-template-file ./packaged.yaml
 sam deploy --profile "${PROFILE}" --region "${REGION}" --template-file ./packaged.yaml --stack-name "${STACK_NAME}" --capabilities CAPABILITY_IAM
--- test.sh
+++ test.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-./load_env.sh
+# check if awscli is installed
+aws --version >> /dev/null || ( echo "awscli not installed" && exit 1 )
 
+# check if samcli is installed
+sam --version >> /dev/null || ( echo "samcli not installed" && exit 1 )
+
+# load the env file
+set -a
+. ./local_config.env || ( echo "local_config.env file doesn't exist, exiting" && exit 0 )
+set +a
+
+# run the test command
 sam local invoke --profile "${PROFILE}" --region "${REGION}" -t ./template.yaml -e ./tests/test_event.json
