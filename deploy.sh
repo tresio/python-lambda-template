81b059275211c3bdd01f40dfcc8902b27bd11985
JJ Ferman <fermanjj@gmail.com>
2019-09-16 09:49:39 -0700

    fixing shell exit commands

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # check if awscli is installed
-aws --version >> /dev/null || ( echo "awscli not installed" && exit 1 )
+aws --version >> /dev/null || ( echo "awscli not installed" && exit 1 ) || exit 1
 
 # check if samcli is installed
-sam --version >> /dev/null || ( echo "samcli not installed" && exit 1 )
+sam --version >> /dev/null || ( echo "samcli not installed" && exit 1 ) || exit 1
 
 # load the env file
 set -a
-. ./local_config.env || ( echo "local_config.env file doesn't exist, exiting" && exit 0 )
+. ./local_config.env || ( echo "local_config.env file doesn't exist, exiting" && exit 1 ) || exit 1
 set +a
 
 # run the deploy steps
