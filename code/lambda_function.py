a0a0d7f04e9321a448503cfb4fa9d31bec4915af
JJ Ferman <fermanjj@gmail.com>
2019-09-23 13:25:25 -0700

    wrapping code in try/catch

--- code/lambda_function.py
+++ code/lambda_function.py
@@ -1,5 +1,12 @@
 
-
-def lambda_handler(event, context):
+def _lambda_handler(event, context):
     # TODO: Implement your code here
     print(event)
+
+
+def lambda_handler(event, context):
+    try:
+        _lambda_handler(event, context)
+    except Exception as e:
+        # do any error handling and logging here
+        raise e
