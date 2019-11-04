2c435a3cf9812454cb92f4fff5f6136e5cc38b4a
JJ Ferman <fermanjj@gmail.com>
2019-11-04 11:11:03 -0800

    making api gateway compatible

--- code/lambda_function.py
+++ code/lambda_function.py
@@ -3,10 +3,13 @@ def _lambda_handler(event, context):
     # TODO: Implement your code here
     print(event)
 
+    # only used in conjunction with API Gateway
+    return {"statusCode": 200}
+
 
 def lambda_handler(event, context):
     try:
-        _lambda_handler(event, context)
+        return _lambda_handler(event, context)
     except Exception as e:
         # do any error handling and logging here
         raise e
